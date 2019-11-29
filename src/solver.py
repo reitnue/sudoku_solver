35a9ae0f6c5e7a1e5e0088d2acbcfdd416feb3b6
Alex Wang <lifengawang@gmail.com>
2019-11-29 12:28:51 -0500

    initial commit

---
+++ src/solver.py
@@ -0,0 +1,9 @@
+import sys
+import json
+
+if __name__ == '__main__':
+    # test1
+    with open('../test/test1.json', 'r') as test1_file:
+        test1 = json.loads(test1_file.read())
+
+    print(test1)
---
+++ src/sudoku.py
@@ -0,0 +1,6 @@
+import sys
+
+class Sudoku:
+	def __init__(self, board=None):
+		if grid:
+			self.board = board # 2d array
