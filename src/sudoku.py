8a7700eb11f1b101f7702a7cc6664c31cb2535e0
Alex Wang <lifengawang@gmail.com>
2019-12-24 14:43:13 -0500

    individual squares method - valid_numbers

--- src/sudoku.py
+++ src/sudoku.py
@@ -3,7 +3,15 @@ import json
 import numpy as np
 
 INDICES = [0, 3, 6]
+NUMBERS = [x for x in range(1, 10)]
+# NUMBERS_SET = set(NUMBERS)
 
+def compute_square(row, col):
+    return ((row // 3) * 3) + (col // 3)
+
+'''
+row, col are both zero-indexed
+'''
 class Sudoku:
     def __init__(self, board=None):
         self.board   = np.array(board) # 2d array
@@ -19,12 +27,54 @@ class Sudoku:
     def __str__(self):    
         return '\n'.join([' '.join(map(str, x)) for x in self.board])
 
+    # return a list of valid numbers for a square (row, col)
+    def valid_numbers(self, row, col):
+        if self.board[row, col] != 0:
+            return [self.board[row, col]]
+        result = set(NUMBERS)
+        result -= self.rows[row]
+        result -= self.cols[col]
+        result -= self.squares[compute_square(row, col)]
+        return sorted(list(result))
+
+    def fill_number(self, row, col, number):
+        if number not in self.valid_numbers(row, col):
+            return -1
+        self.board[row, col] = number
+        self.rows[row].add(number)
+        self.cols[col].add(number)
+        self.squares[compute_square(row, col)].add(number)
+        return 0
+
+    def is_filled(self, row, col):
+        return self.board[row, col] != 0
+
+    def is_completed(self):
+        result = True
+        result &= all(map(len, self.rows))
+        result &= all(map(len, self.cols))
+        result &= all(map(len, self.squares))
+        return result
 
 if __name__ == '__main__':
     with open('../test/test1.json', 'r') as test1_file:
         test1 = json.loads(test1_file.read())
     temp = Sudoku(test1)
     print(temp)
-    print(temp.rows)
-    print(temp.cols)
-    print(temp.squares)
+    
+    print('-----------------')
+
+    print('rows', temp.rows)
+    print('cols', temp.cols)
+    print('squares', temp.squares)
+
+    print('-----------------')
+    for _ in range(2):
+        for i in range(9):
+            for j in range(9):
+                if not temp.is_filled(i, j):
+                    if len(temp.valid_numbers(i, j)) == 1:
+                        temp.fill_number(i, j, temp.valid_numbers(i, j)[0])
+        print(temp)
+        print('-----------------')
+    print(temp.is_completed())
