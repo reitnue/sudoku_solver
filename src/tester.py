742053372249acdc1230bdcb76ede697852ad09c
Alex Wang <lifengawang@gmail.com>
2019-12-24 20:29:19 -0500

    added tests and a tester

---
+++ src/tester.py
@@ -0,0 +1,28 @@
+import sys
+import json
+from sudoku import Sudoku
+# from solver import ...?
+
+def tester(number, solver):
+	board = get_test_board(number)
+	# solved = get_solved_board(number)
+	game = Sudoku(board)
+	solver(game)
+	return game.is_completed()
+
+def get_test_board(number):
+	with open('../test/test{:02}.json'.format(number), 'r') as test1_file:
+        test1 = json.loads(test1_file.read())
+    return test1
+
+def get_solved_board(number):
+	with open('../test/test{:02}_solved.json'.format(number), 'r') as test1_file:
+        test1 = json.loads(test1_file.read())
+    return test1
+
+if __name__ == '__main__':
+    # test1
+    with open('../test/test01.json', 'r') as test1_file:
+        test1 = json.loads(test1_file.read())
+
+    temp = Sudoku(test1)
