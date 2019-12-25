bf6c54526209572c231d37d6a0bb4b0e2bfeaf61
Alex Wang <lifengawang@gmail.com>
2019-12-24 21:26:58 -0500

    tester to pull from sugoku and database of easy puzzles

--- src/tester.py
+++ src/tester.py
@@ -1,11 +1,18 @@
 import sys
 import json
+import requests
+
 from sudoku import Sudoku
 from solver import cellwise
 
-def tester(number, solver):
+def tester_number(number, solver):
     board = get_test_board(number)
     # solved = get_solved_board(number)
+    game = Sudoku(board)
+    solver(game)
+    return game.is_completed()
+
+def tester_board(board, solver):
     game = Sudoku(board)
     solver(game)
     return game.is_completed()
@@ -20,11 +27,22 @@ def get_solved_board(number):
         test1 = json.loads(test1_file.read())
     return test1
 
+'''
+level: easy, medium, hard, random
+'''
+def get_sugoku_board(level):
+    req = requests.get("https://sugoku.herokuapp.com/board?difficulty={}".format(level))
+    return json.loads(req.text)['board']
+
 if __name__ == '__main__':
     for num in range(1, 4):
-        print(num, tester(num, cellwise))
-    # test1
-    # with open('../test/test01.json', 'r') as test1_file:
-    #     test1 = json.loads(test1_file.read())
-
-    # temp = Sudoku(test1)
+        print(num, tester_number(num, cellwise))
+    
+    num_test = 10
+    count = 0
+    for _ in range(num_test):
+        temp_board = get_sugoku_board('easy')
+        print(temp_board)
+        if tester_board(temp_board, cellwise):
+            count += 1
+    print(count / num_test)
